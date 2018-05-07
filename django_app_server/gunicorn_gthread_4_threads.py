3202ca24479746c41cbdbbbc6bbb4ddec174bb6c
Mateusz Paciorek <mpaciorek@grupainteger.pl>
2018-05-07 11:56:43 +0200

    fix

--- django_app_server/gunicorn_gthread_4_threads.py
+++ django_app_server/gunicorn_gthread_4_threads.py
@@ -1,8 +1,6 @@
 # KONFIGURACJA WIELOWĄTKOWA Z 4 wątkami na proces
 #
-
 bind = "127.0.0.1:8100"
 workers = 4
 preload_app = True
-worker_class = "gevent"
 threads = 4
--- django_app_server/gunicorn_gthread_8_threads.py
+++ django_app_server/gunicorn_gthread_8_threads.py
@@ -3,5 +3,4 @@
 bind = "127.0.0.1:8100"
 workers = 4
 preload_app = True
-worker_class = "gevent"
 threads = 4
